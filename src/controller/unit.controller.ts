b6a8f07dea39e866584dc67410d2be2be8239aac
Aria Adi Pramesta <ariaapramesta@gmail.com>
2026-03-13 13:09:12 +0700

    feat: update status validation

--- src/controller/unit.controller.ts
+++ src/controller/unit.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 
 import { Status } from "../../generated/prisma/enums";
+import { validateStatusUpdate } from "../utils/statusValidation";
 
 export const createUnit = async (req: Request, res: Response) => {
   try {
@@ -91,21 +92,43 @@ export const deleteUnit = async (req: Request, res: Response) => {
 export const updateUnit = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    const { status } = req.body;
+    const { newStatus } = req.body;
 
-    const unit = await prisma.unit.update({
+    const unit = await prisma.unit.findUnique({ where: { id: id as string } });
+
+    if (!unit)
+      return res
+        .status(404)
+        .json({ success: false, message: "Unit not found" });
+
+    const validationResult = validateStatusUpdate(
+      unit.status,
+      newStatus as Status,
+    );
+
+    if (!validationResult.isValid) {
+      return res.status(400).json({
+        success: false,
+        message: validationResult.error,
+      });
+    }
+
+    const updatedUnit = await prisma.unit.update({
       where: { id: id as string },
-      data: { ...(status !== undefined && { status }) },
+      data: {
+        status: newStatus as Status,
+      },
     });
 
     res.json({
       success: true,
-      data: unit,
+      message: "Status unit berhasil diperbarui",
+      data: updatedUnit,
     });
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: "Failed to update post",
+      message: "Failed to update unit",
       error: error instanceof Error ? error.message : "Unknown error",
     });
   }
